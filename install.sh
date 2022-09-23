7de87f61df6fa9f6ad94a1db3ea7247cb49a70bd
ramallahos <110548111+ramallahos@users.noreply.github.com>
2022-09-23 04:10:52 +0000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,3 @@
+    git clone --depth 1 --single-branch https://github.com/doomemacs/doomemacs /home/$USER/.config/emacs
+    /home/$USER/.config/emacs/bin/doom install
+    cp -r /etc/skel/.doom.d/ $HOME/.doom.d/
